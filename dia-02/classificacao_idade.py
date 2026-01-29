e916d4437bc43a3510aa3ed3d228a452f5379499
filipe-gomes-py <filipegomespsicologo@gmail.com>
2026-01-29 09:42:35 -0300

    dia 02 - task 01 - classificacao de idade

---
+++ dia-02/classificacao_idade.py
@@ -0,0 +1,9 @@
+idade = int(input("Qual sua idade?"))
+if idade <13:
+    print("Criança")
+elif idade <18:
+    print("Adolescente")
+elif idade <60:
+    print("Adulto")
+else:
+    print("Idoso")
